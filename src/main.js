674544ee6bb23607929e6359bebf3008dc094d0f
jeehoo0767 <jeehoo0767@naver.com>
2022-03-13 23:54:23 +0900

    최신화

--- src/main.js
+++ src/main.js
@@ -1,35 +1,35 @@
-require("dotenv").config();
-import Koa from "koa";
-import Router from "koa-router";
-import bodyParser from "koa-bodyparser";
-import mongoose from "mongoose";
-import cors from "@koa/cors";
-import api from "./api";
-import socketManager from "./lib/socketManager";
+require('dotenv').config();
+import Koa from 'koa';
+import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
+import mongoose from 'mongoose';
+import cors from '@koa/cors';
+import api from './api';
+import socketManager from './lib/socketManager';
 
 const { PORT, MONGO_URI } = process.env;
 
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true })
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log('Connected to MongoDB');
   })
   .catch((e) => {
     console.error(e);
   });
 
 const app = new Koa();
-const server = require("http").Server(app.callback());
-export const io = require("socket.io")(server, { cors: { origin: "*" } });
+const server = require('http').Server(app.callback());
+export const io = require('socket.io')(server, { cors: { origin: '*' } });
 
 const router = new Router();
 
 app.use(
   cors({
     // host: 'http://localhost:4000',
-    origin: "http://localhost:3000",
+    origin: 'http://localhost:3000',
     // credentials: true,
-  })
+  }),
 );
 
 /**
@@ -39,19 +39,19 @@ app.use(
 app.use(bodyParser());
 
 // 라우터 설정
-router.use("/api", api.routes());
+router.use('/api', api.routes());
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
-const serve = require("koa-static");
+const serve = require('koa-static');
 // 이미지 파일 인식을 위해 static경로 설정
-app.use(serve("."));
+app.use(serve('.'));
 // console.log(__dirname + "./api/post/uploads/");
 
 const port = PORT || 4000;
 server.listen(port, () => {
-  console.log("Listening to port %d", port);
+  console.log('Listening to port %d', port);
 });
 
-io.on("connection", socketManager);
+io.on('connection', socketManager);
